8b7d3589aee10771f4f063686d8b0f48a32d87c9
Jerrell Prier <jerrellprier@gmail.com>
2021-11-23 16:51:00 -0600

    Styled the calculator a bit. Added more buttons.

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const input = document.querySelector('input');
+
 document.querySelector('#add').onclick = () => input.value+='+';
+document.querySelector('#subtract').onclick = () => input.value+='-';
 document.querySelector('#multiply').onclick = () => input.value+='*';
+document.querySelector('#divide').onclick = () => input.value+='/';
 document.querySelector('#clr').onclick = () => input.value='';
 document.querySelector('#decimal').onclick = () => input.value+='.';
+
 document.querySelector('#one').onclick = () => input.value+='1';
 document.querySelector('#two').onclick = () => input.value+='2';
 document.querySelector('#three').onclick = () => input.value+='3';
@@ -13,6 +17,5 @@ document.querySelector('#seven').onclick = () => input.value+='7';
 document.querySelector('#eight').onclick = () => input.value+='8';
 document.querySelector('#nine').onclick = () => input.value+='9';
 document.querySelector('#zero').onclick = () => input.value+='0';
-
 
 document.querySelector('#equal').onclick = () => input.value = eval(input.value);
